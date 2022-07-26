938539f713259b6b5f69cd15fac309cda76b556c
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-26 07:58:15 -0700

    Update

---
+++ Acme Acounting/Acme Acounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme_Acounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
