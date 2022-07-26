31891cc61f0f180e8917dd77df08f7618ca8fb09
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-26 09:33:24 -0700

    Update

---
+++ Daily Report/Daily Report/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report\n");
+            Console.WriteLine("What is your name?"); //Writes a Line in the program asking a question.
+            string yourName = Console.ReadLine(); // Stores the typed string into the variable yourName
+            Console.WriteLine("\nWhat course are you on?");
+            string courseName = Console.ReadLine(); // Stores the typed string into the variable courseName 
+            Console.WriteLine("\nWhat page number?");
+            string pageNumber = Console.ReadLine();
+            Console.WriteLine("\nDo you need help with anything? Please answer \"true\" or \"false\".");
+            string help = Console.ReadLine(); // Stores the typed string into the variable help
+            Console.WriteLine("\nWere there any positve expreinces you'd like to share? Please be specific.");
+            string positiveExperience = Console.ReadLine();// Stores the typed string into the variable positiveExpeirence
+            Console.WriteLine("\nIs there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();// Stores the typed string into the variable feedback
+            Console.WriteLine("\nHow many hours did you study today?");
+            string hoursStudied = Console.ReadLine(); // Stores the typed string into the variable hoursStudied
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+        }
+    }
+}
