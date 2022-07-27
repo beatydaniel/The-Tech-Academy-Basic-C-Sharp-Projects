7a3c8c2a289e1df1d8d443eafb5feee6bcd0852a
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-27 08:40:48 -0700

    Update

---
+++ Console App Submission/Console App Submission/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Console_App_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the letter?");
+            string letter = Console.ReadLine();
+            bool isGuessed = letter == "u"; // is true if the typed letter is u
+
+            do //does the loop after the first input
+            {
+                switch (letter)
+                {
+                    case "a":
+                        Console.Write("You guessed a. Try again. ");
+                        Console.WriteLine("Guess a letter?");
+                        letter = Console.ReadLine();
+                        break;
+                    case "f":
+                        Console.WriteLine("You guessed f. Try again. ");
+                        Console.WriteLine("Guess a letter?");
+                        letter = Console.ReadLine();
+                        break;
+                    case "u":
+                        Console.WriteLine("You guessed u. Thats correct!");
+                        isGuessed = true; //this stops the loop.
+                        break;
+                    default:
+                        Console.WriteLine("Your Wrong. Try again. ");
+                        Console.WriteLine("Guess a letter?");
+                        letter = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!isGuessed); //Starts the loop
+            Console.Read();
+        }
+    }
+}
