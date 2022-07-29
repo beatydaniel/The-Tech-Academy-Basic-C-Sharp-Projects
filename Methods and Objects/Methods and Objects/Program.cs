c8cd1ba07e78276670937ea4d94a4a822aba42b3
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-29 16:01:57 -0700

    Update

---
+++ Methods and Objects/Methods and Objects/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Methods_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { FirstName = "Sample", LastName = "Student" }; // Employee class inherits from the Person class allowing me to use Person's properties
+            employee.SayName(); //Calls the  superclass on the Employee Object
+            Console.ReadLine();
+        }
+    }
+}
