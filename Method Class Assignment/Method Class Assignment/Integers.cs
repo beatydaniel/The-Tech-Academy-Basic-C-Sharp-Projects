f134f577753b0f676e32a43811caf6d28b875340
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-29 12:27:58 -0700

    Update

---
+++ Method Class Assignment/Method Class Assignment/Integers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Class_Assignment
+{
+    public class Integers
+    {
+        public void Integer(int first, int second)
+        {
+            int number = first + 10;
+
+            Console.WriteLine(second); // Prints the second integer to the console
+        }
+
+    }
+}
---
+++ Method Class Assignment/Method Class Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Class_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers method = new Integers(); //Instantiating the class
+
+            method.Integer(first: 3, second: 5);  // Passing in two numbers and specifying the parameters
+
+            Console.ReadLine();
+        }
+    }
+}
