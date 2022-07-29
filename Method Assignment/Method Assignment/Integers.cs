91a582499cc4fc7315bae702ae8d27863e4ac46a
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-29 11:54:54 -0700

    Update

---
+++ Method Assignment/Method Assignment/Integers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    public class Integers
+    {
+        public int Integer(int first, int second = 20) // a input number and another input or if does not exists uses 20
+        {
+            return first + second; // adds the two integers
+        }
+    }
+}
---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integers method = new Integers();
+            Console.WriteLine("Please Enter a Number: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease Enter a Second Number(Optional- Press Enter to leave blank): ");
+            string stringNumber = Console.ReadLine();
+
+            if (stringNumber == "") // if the value of the second input is blank do this
+            {
+                Console.WriteLine(numberOne + " + 20 " + " = " + method.Integer(numberOne));
+                Console.ReadLine();
+            }
+            else // if there is a input of a integer do this
+            {
+                int numberTwo = Convert.ToInt32(stringNumber);
+                Console.WriteLine(numberOne + " + " + numberTwo + " = " + method.Integer(numberOne, numberTwo)); // if there are two numbers entered add them using the method integers
+                Console.ReadLine();
+            }  
+        }
+    }
+}
