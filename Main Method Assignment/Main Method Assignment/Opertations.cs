e88b5bc66e75cdb9f3fef904310c2b942e9a8848
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-29 10:01:36 -0700

    Update

---
+++ Main Method Assignment/Main Method Assignment/Opertations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+   public class Opertations
+    {
+        public int Operation(int awnser)  // Uses a integar.
+        {
+            return awnser + 10; // Gives the value that is from the main program and adds it by 10
+        }
+        public int Operation(decimal decimalNum)
+        {
+            int awnser = Convert.ToInt32(decimalNum) * 25; // Converts the decimal into a integer
+            return awnser;
+        }
+        public int Operation(string number) // takes a string
+        {
+            int awnser = Convert.ToInt32(number) - 30; //converts the string into a integer
+            return awnser;
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opertations method = new Opertations(); // Calls the Class Operations containing the methods.
+
+            int awnser = method.Operation(300); // Using a integer and adding using the method
+            Console.WriteLine(awnser + " - This is from the first method:" + " 300 + 10");
+            Console.ReadLine();
+
+            int awnser2 = method.Operation(300.5m); // Using a decimal and is converted using the method
+            Console.WriteLine(awnser2 + " - This is from the second method:"+ " 300.5 * 25 ");
+            Console.ReadLine();
+
+            int awnser3 = method.Operation("300"); // Using a string and is converted using the method
+            Console.WriteLine(awnser3 + " - This is from the third method:" + " \"300\"- 30");
+            Console.ReadLine();
+        }
+    }
+}
