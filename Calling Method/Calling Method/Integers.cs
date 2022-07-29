3341c112c9c49b026013cfa251de2f6797ea6170
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-07-28 17:20:03 -0700

    Update

---
+++ Calling Method/Calling Method/Integers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Method
+{
+    public class Integers
+    {
+       public int Add(int number) //Method - Adds the input by the methods value
+        {
+            int awnser = number + 100;
+            return awnser;
+        } 
+        public int Subtraction(int number) //Method - Subtracts by using the input by the methods value
+        {
+            int awnser = number - 50;
+            return awnser;
+        }
+        public int Multiply(int number) //Method - Multiplies the input by the methods value
+        {
+            int awnser = number * 20;
+            return awnser;
+        }
+    }
+}
---
+++ Calling Method/Calling Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a number to do a math operation: ");
+            int num = Convert.ToInt32(Console.ReadLine()); // stores the users input
+
+            Integers method = new Integers(); // Calls the Class Integers containing the methods.
+
+            Console.WriteLine("\n"+ num + " + 100 = " + method.Add(num));
+            Console.ReadLine();
+            
+            Console.WriteLine("\n"+ num + "- 50 = " + method.Subtraction(num));
+            Console.ReadLine();
+           
+            Console.WriteLine("\n"+ num + " * 20 = " + method.Multiply(num));
+            Console.ReadLine();
+
+        }
+    }
+}
