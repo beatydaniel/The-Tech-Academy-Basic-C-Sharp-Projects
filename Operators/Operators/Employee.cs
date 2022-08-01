668a243e7dda7853fead556e63d346d8368a6d13
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-01 14:16:03 -0700

    Update

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operators
+{
+    public class Employee
+    {
+        public int ID { get; set; } // My properties
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public static bool operator == (Employee employee1, Employee employee2) // Creating a true or false bool using the == operator
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool operator != (Employee employee1, Employee employee2) // Creating a true or false bool using the != operator
+        {
+            if (employee1.ID != employee2.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { firstName = "Sample", lastName = "Student", ID = 1234 };
+            Employee employee1 = new Employee() { firstName = "Sample2", lastName = "Student2", ID = 1234};
+            Console.WriteLine(employee.firstName + " " + employee.lastName+ " " + employee.ID);
+            Console.WriteLine(employee1.firstName + " " + employee1.lastName +" " + employee1.ID);
+            Console.ReadLine();
+
+            if(employee == employee1)
+            {
+                Console.WriteLine("The IDs of the two employees are the same");
+            }
+            else
+            {
+                Console.WriteLine("The IDs are unique");
+            }
+        }
+    }
+}
