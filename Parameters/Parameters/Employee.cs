00888e24a13a90397b738f9c37eb175f56791fa1
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-01 15:26:43 -0700

    Update

---
+++ Parameters/Parameters/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parameters
+{
+    public class Employee <T> //Generic parameter
+    {
+        public List<T> things { get; set; }
+    }
+}
---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> name = new Employee<string>();
+            name.things = new List<string> { "Daniel", "Brian", "Alex" };
+
+            foreach (string thing in name.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Employee<int> number = new Employee<int>();
+            number.things = new List<int> { 1, 2, 3 };
+
+            foreach (int thing2 in number.things)
+            {
+                Console.WriteLine(thing2);
+            }
+            Console.ReadLine();
+        }
+    }
+}
