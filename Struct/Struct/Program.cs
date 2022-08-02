94160bf395b27470b84c9863751bcd1a37cd151b
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-01 18:35:56 -0700

    Update

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        public struct Number //This is a stuct
+        {
+            public decimal Amount; // Here i set its datatype to be a decimal
+
+        }
+
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 100.75m; // Here we can set the amount in decimal by using the struct
+
+            Console.WriteLine("This is the amount = " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
