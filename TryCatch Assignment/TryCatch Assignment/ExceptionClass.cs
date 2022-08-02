213e61db78eaf55f005b2ad967286a652bace1b4
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-02 16:22:11 -0700

    Update

---
+++ TryCatch Assignment/TryCatch Assignment/ExceptionClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TryCatch_Assignment
+{
+    public class ExceptionClass : Exception
+    {
+        public ExceptionClass()
+            : base() { }
+        public ExceptionClass(string message)
+            : base(message) { }
+    }
+}
---
+++ TryCatch Assignment/TryCatch Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TryCatch_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your age: ");
+            try
+            {
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age <= 0) // if the input is anything other than a whole number does this.
+                {
+                    throw new ExceptionClass(); //Instatiating the class
+                }
+                DateTime currentTime = DateTime.Now;
+                int currentYear = currentTime.Year; //Takes the current times year and sets it
+                int birthYear = currentYear - age; // takes the users input and subtracts it from the current year
+
+                Console.WriteLine("You were born in:"+ birthYear); 
+                Console.ReadLine();
+            }
+            catch (ExceptionClass)
+            {
+                Console.WriteLine("Please do not enter 0 or a negative number only whole numbers. ");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong!");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
