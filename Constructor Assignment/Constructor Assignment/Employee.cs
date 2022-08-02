10a69570fa6045cd5f60a9c7a7a2efa8065e3520
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-02 15:20:36 -0700

    Update

---
+++ Constructor Assignment/Constructor Assignment/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor_Assignment
+{
+    public class Employee
+    {
+        public Employee() : this("Daniel", "Beaty") //First Constructor using 'this' lets another Constructor use this if the parameters are left blank
+        {
+        }
+        public Employee(string firstName, string lastName) // Second Constructor
+        {
+            fName = firstName;
+            lName = lastName;
+        }
+        public string fName { get; set; }
+        public string lName { get; set; }
+    }
+}
---
+++ Constructor Assignment/Constructor Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructor_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Daniel Beaty";
+            var program = "\"is using C#\"";
+            Console.WriteLine(myName + " is a const string and can never change and " + "{0} is coming from a var instead of string", program);
+            Console.ReadLine();
+
+            Employee constuctor = new Employee(); //Instantiating  my class Person that has a chain constructor.
+
+            Console.WriteLine(constuctor.fName + " " + constuctor.lName + ". This is coming from my constructor");
+            Console.ReadLine();
+        }
+    }
+}
