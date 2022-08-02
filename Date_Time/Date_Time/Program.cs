945fea43db12ea3e88bb4f4d5a53935b3d9c8b53
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-02 12:53:20 -0700

    Update

---
+++ Date_Time/Date_Time/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now; // Gets the Current Time
+            Console.WriteLine(time); // Displays the Current Time
+            Console.WriteLine("\n Please Enter a Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            DateTime addTime = time.AddHours(num); // Takes the input number and adds the number to the current time
+            Console.WriteLine(addTime);
+            Console.ReadLine();
+        }
+    }
+}
