3db708305fbc8f3e16f571fd414e4e8d85332555
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-03 09:28:59 -0700

    Update

---
+++ TwentyOne/Casino/Deck.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+
+            for (int i = 0; i < 13; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)i;
+                    card.Suit = (Suit)j;
+                    Cards.Add(card);
+                }
+            }
+        }
+        public List<Card>Cards { get; set; }
+
+        public void Shuffle(int times = 1) //Method
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+
+                while (Cards.Count > 0)
+                {
+                    int randomIndex = random.Next(0, Cards.Count); //creates a random number between 0 and 52
+                    TempList.Add(Cards[randomIndex]); //Adds the Card to the tempList
+                    Cards.RemoveAt(randomIndex); // Deleted it from the list
+                }
+               Cards = TempList;
+            }
+        }
+    }
+}
