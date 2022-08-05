3c5bbb6e02999a1f44b1604943e776d99099c20c
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-08-05 09:35:50 -0700

    Update

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var users = db.Insurees;
+                var userVms = new List<InsureeVm>();
+                foreach (var user in users)
+                {
+                    var userVm = new InsureeVm();
+
+                    userVm.FirstName = user.FirstName;
+                    userVm.LastName = user.LastName;
+                    userVm.EmailAddress = user.EmailAddress;
+                    userVm.Quote = user.Quote;
+                    userVms.Add(userVm);
+                }
+                return View(userVms);
+            }
+        }
+    }
+}
